8a6143a607b1f44854fbde57035d05e033b7a88b
WinstonMathamba <winstonmathamba@gmail.com>
2016-08-03 14:48:58 -0400

    first Commit for Day 3 assignment

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Created by win808mac on 8/3/16.
+ */
+public class Main {
+    /* I created a Main class above */
+}
+    public static void main(String[] args) {
+        /* I created a Main method and i'm about to write the method code
+
+         */
+        int myAge = 39;
+        String myName = ("WinstonM");
+        int numberOfKids = 1;
+        String kidsName = ("Julian");
+        String courseName = ("Java");
+        int courseLength = 3;
+        int totalFingersLeftHand = 05;
+        int totalFingersRightHand = 05;
+        int totalEyeballs = 2;
+        boolean awesome = true;
+
+    }
+}
